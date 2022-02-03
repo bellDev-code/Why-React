3591714cb1b6f1a5bfb789c62585a5eb3c61d196
bellDev-code <sumaoo20@naver.com>
2022-02-03 16:12:55 +0900

    React useEffect !

--- react-beginners/src/App.js
+++ react-beginners/src/App.js
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
 
   const onClick = () => setValue((prev) => prev + 1);
 
-  // 1. 만약 API를 불러온다는 가정하에 현재 state가 바뀔때마다 계속 'call an API' 가 불러와진다.
-  // 2. state를 변경할 때 모든 code들은 다시 시작된다. 때로는 이 현상이 좋으나, 처음 딱 한 번만 실행되고 다시는 실행되지 않는걸 원할 수 있다.
-  console.log('call an API');
+  // 만약 API를 불러온다는 가정하에 현재 state가 바뀔때마다 계속 'call an API' 가 불러와진다.
+  // state를 변경할 때 모든 code들은 다시 시작된다. 때로는 이 현상이 좋으나, 처음 딱 한 번만 실행되고 다시는 실행되지 않는걸 원할 수 있다.
+
+  // 계속해서 실행되는 코드
+  console.log('I run all the time');
+
+  // 한 번만 실행되는 코드
+  // 1. 해결책
+  // const iRunOnlyOnece = () => {
+  //   console.log('I run only once');
+  // };
+
+  // 1. 해결책
+  // useEffect(iRunOnlyOnece, []);
+
+  // 2.해결책 한 번만 호출하게 한다.
+  useEffect(() => {
+    console.log('CALL THE API...');
+  }, []);
+
   return (
     <div>
       <h1>{counter}</h1>
