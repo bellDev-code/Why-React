9176acb7c1b77cea35cc75e51303ec92b1e4b12c
bellDev-code <sumaoo20@naver.com>
2022-02-03 17:23:53 +0900

    Recap useEffect

--- react-beginners/src/App.js
+++ react-beginners/src/App.js
@@ -10,18 +10,26 @@ function App() {
 
   console.log('I run all the time');
 
+  // 코드를 언제 실행할지 결정하는 함수
+  //
   useEffect(() => {
+    // 처음 한 번만 실행되는 코드
     console.log('I run only once.');
   }, []);
 
   useEffect(() => {
+    // keyword를 주시해라! keywor가 변경되면 코드가 실행
     console.log("I run when 'keyword' chnages. ");
-    // keyword를 주시해라!
   }, [keyword]);
 
   useEffect(() => {
     console.log("I run when 'counter' changes");
   }, [counter]);
+
+  useEffect(() => {
+    // keyword, counter 중 둘 중 하나가 변경되면 실행하는 코드
+    console.log('I run when keyword & counter changes');
+  }, [keyword, counter]);
 
   return (
     <div>
